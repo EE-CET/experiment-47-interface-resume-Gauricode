13fd3b42501c39d7914635ae5a7563f1c255f0a3
unknown <gaurijkjay@gmail.com>
2026-02-06 17:15:31 +0530

    check

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -2,21 +2,34 @@ import java.util.Scanner;
 
 interface Resume {
     // TODO: Declare method void biodata();
+	void biodata();
 }
 
 class Teacher implements Resume {
     // Attributes
-   
+   String name,qualification;
+   int experience;
+   public void biodata(){
+	System.out.println("Name: " + name);
+	System.out.println("Qualification: " + qualification);
+	System.out.println("Experience: " + experience);
+    }
 
     // TODO: Implement biodata() method to print details in the required format
 }
 
 public class InterfaceResume {
-   
-        
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	Teacher t=new Teacher();
+	t.name=sc.nextLine();
+	t.qualification=sc.nextLine();
+        t.experience=sc.nextInt();
+	sc.nextLine();
         // TODO: Read input (Name, Qualification, Experience)
         // Assign to teacher object attributes
-        
+	
+        t.biodata();
         // TODO: Call teacher.biodata()
-    
+    }
 }
